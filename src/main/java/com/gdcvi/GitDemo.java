2578124b132ed7912d30cdbce47fb3bf243cddda
admin <admin@qq.com>
2022-05-29 21:03:41 +0800

    IDEA github commit

--- src/main/java/com/gdcvi/GitDemo.java
+++ src/main/java/com/gdcvi/GitDemo.java
@@ -8,6 +8,7 @@ public class GitDemo {
         System.out.println("hello git4");
         System.out.println("master");
         System.out.println("hot fix");
+        System.out.println("hot fix2");
 
     }
 }
